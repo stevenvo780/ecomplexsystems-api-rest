d63259b8e68845ca93f5aa23b6f979d294f054a5
steven <stevenvallejo780@gmail.com>
2020-10-11 13:22:00 -0500

    git init

---
+++ app/Http/Controllers/ProcessExecutionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProcessExecution;
+use Illuminate\Http\Request;
+
+class ProcessExecutionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProcessExecution  $processExecution
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProcessExecution $processExecution)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProcessExecution  $processExecution
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProcessExecution $processExecution)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProcessExecution  $processExecution
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProcessExecution $processExecution)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProcessExecution  $processExecution
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProcessExecution $processExecution)
+    {
+        //
+    }
+}
